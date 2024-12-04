edfd7d8552adf7f5091f6e61ddc9935f11a4b956
ruhai-l <ruhailin19@outlook.com>
2024-12-03 23:06:59 -0500

    merge to main directly from branch

--- Route App/test.py
+++ Route App/test.py
@@ -1,3 +1,6 @@
 print("Welcome to the Route App. This is my smart plug-in app for Google Chrome!");
 print("Testing a branch merge");
 print("Another test");
+print(
+    "merge directly to main"
+);
